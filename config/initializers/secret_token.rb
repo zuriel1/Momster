664ed131b19e00f2f8f9697f8b50ee605a310c9e
vagrant <zuriel.wz@gmail.com>
2015-08-18 15:46:37 +0000

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Momster::Application.config.secret_key_base = '6bc5fb692f6a9f67203539f6147c3af398ec3a96b304a595dc097daed66162f39446ed8a535888b4a75e0290cc4e8056ca3d747053c2b451affa94a7a11f9f41'
