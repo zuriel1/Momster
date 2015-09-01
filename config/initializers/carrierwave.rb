148f022e411b04c3a58d9ce95bcf1c353d259ca9
vagrant <zuriel.wz@gmail.com>
2015-09-01 17:31:17 +0000

    location

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,9 +4,8 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => ENV["AWS_ACCESS_KEY"],                        # required
-    :aws_secret_access_key  => ENV["AWS_SECRET_KEY"]  
-        :region => 'us-west-2'
-                       # required
+    :aws_secret_access_key  => ENV["AWS_SECRET_KEY"],                       # required
+    :region => 'us-west-2'
   }
   config.fog_directory  = ENV["AWS_BUCKET"]                     # required
 end
